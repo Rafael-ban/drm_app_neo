72cc16675b2311c0f980480502e0aa9c3e8c6626
Shirogane Tsumugi <inapp@iccmc.cc>
2026-01-05 01:23:12 +0800

    修复encoder模式按钮导航会自动进入编辑模式的问题

--- src/driver/key_enc_evdev.c
+++ src/driver/key_enc_evdev.c
@@ -26,21 +26,43 @@ static int key_enc_evdev_process_key(uint16_t code)
     }
 }
 
+static void print_lv_key_str(int key){
+    switch(key){
+        case LV_KEY_LEFT:
+            log_debug("LV_KEY_LEFT");
+            break;
+        case LV_KEY_RIGHT:
+            log_debug("LV_KEY_RIGHT");
+            break;
+        case LV_KEY_ENTER:
+            log_debug("LV_KEY_ENTER");
+            break;
+        case LV_KEY_ESC:
+            log_debug("LV_KEY_ESC");
+            break;
+        default:
+            log_debug("unknown key: %d", key);
+    }
+}
+
+// 官方文档中说只需要在key_pressed的时候回传data->key
+// 实际上松开的事件也需要回传，否则默认的事件是“放开了enter按钮”，导致误触发选择。
+// 此外 evdev只会在按钮按下/松开的时候产生数据（被read），因此需要缓存数据。
 static void key_enc_evdev_read_cb(lv_indev_t * indev, lv_indev_data_t * data){
+
+    static int last_key = 0;
+    static int last_state = LV_INDEV_STATE_RELEASED;
 
     key_enc_evdev_t * key_enc_evdev = (key_enc_evdev_t *)lv_indev_get_driver_data(indev);
     struct input_event in = { 0 };
-    // while(1){
+    while(1){
         int bytes_read = read(key_enc_evdev->evdev_fd, &in, sizeof(in));
         // log_debug("key_enc_evdev_read_cb: bytes_read = %d, type = %d, code = %d, value = %d", bytes_read, in.type, in.code, in.value);
         if(bytes_read <= 0){
-            return;
+            goto return_with_last_value;
         }
         if(in.type != EV_KEY){
-            return;
-        }
-        if(in.code != KEY_1 && in.code != KEY_2 && in.code != KEY_3 && in.code != KEY_4){
-            return;
+            continue;
         }
 
         if(in.value == 1) {
@@ -50,10 +72,25 @@ static void key_enc_evdev_read_cb(lv_indev_t * indev, lv_indev_data_t * data){
             data->state = LV_INDEV_STATE_PRESSED;
             // data->continue_reading = true;
             key_enc_evdev->input_cb(data->key);
+            last_key = data->key;
+            last_state = data->state;
+            // print_lv_key_str(data->key);
+            // log_debug("key_enc_evdev_read_cb: key = %d pressed", data->key);
+            return;
         } else {
             data->state = LV_INDEV_STATE_RELEASED;
+            data->key = last_key;
+            last_state = data->state;
+            // print_lv_key_str(data->key);
+            // log_debug("key_enc_evdev_read_cb:  release", data->key);
+            return;
         }
-    // }
+    }
+
+return_with_last_value:
+    data->key = last_key;
+    data->state = last_state;
+    return;
 }
 
 lv_indev_t * key_enc_evdev_init(key_enc_evdev_t * key_enc_evdev){
